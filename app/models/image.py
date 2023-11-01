0affb12d0f426dbca9aba8d53eee08ad848a98ec
Jon <jondiezv@gmail.com>
2023-11-01 17:17:44 -0400

    forgot environment variable in models

--- app/models/image.py
+++ app/models/image.py
@@ -3,6 +3,9 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Image(db.Model):
     __tablename__ = 'images'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('posts.id')), primary_key=True)
     url = db.Column(db.String(255), nullable=False)
 
--- app/models/like.py
+++ app/models/like.py
@@ -3,6 +3,9 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Like(db.Model):
     __tablename__ = 'likes'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True)
     post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('posts.id')), primary_key=True)
     updownvote = db.Column(db.Integer, nullable=True)
