14560fff3b7dea8eabe0f1bd011fc48e26d2b78a
Jon <jondiezv@gmail.com>
2023-11-02 12:53:54 -0400

    styled navbar

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -26,3 +26,26 @@ def get_all_posts():
         posts_list.append(post_dict)
 
     return jsonify(posts_list)
+
+
+
+@posts.route('/<int:post_id>', methods=['GET'])
+def get_post_by_id(post_id):
+
+        post = Post.query.get(post_id)
+        if post is None:
+            return jsonify({'error': 'Post not found'}), 404
+
+        image_urls = [image.url for image in post.images]
+
+        post_dict = {
+            'id': post.id,
+            'user_id': post.user_id,
+            'title': post.title,
+            'description': post.description,
+            'hidden': post.hidden,
+            'views': post.views,
+            'image_urls': image_urls,
+        }
+
+        return jsonify(post_dict)
